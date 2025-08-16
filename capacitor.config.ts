02366eaaad27d9383f17a8b3e83b412e2bd9c87c
Jeffrey <ubmjeffer@gmail.com>
2025-08-16 22:34:16 +0300

    REmoved electron

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.constructly.ai',
-  appName: 'Constructly - AI construction agent',
-  webDir: 'dist'
-};
-
-export default config;
